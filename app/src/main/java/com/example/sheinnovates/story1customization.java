bc7b689143353d5f018981d3ddd11ab4de7179d6
Lucy Chen <yic76@pitt.edu>
2019-01-26 16:14:01 -0500

    Created story background page
    Signed-off-by: Lucy Chen <yic76@pitt.edu>

---
+++ app/src/main/java/com/example/sheinnovates/story1customization.java
@@ -0,0 +1,13 @@
+package com.example.sheinnovates;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class story1customization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story1customization);
+    }
+}
