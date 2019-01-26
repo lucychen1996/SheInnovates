767ea53ee9f686cb12d1a0dd859a51ffbe4bb3ff
Qing Chen <qic28@pitt.edu>
2019-01-26 17:47:34 -0500

    More stuff

--- app/src/main/java/com/example/sheinnovates/CreateAccount.java
+++ app/src/main/java/com/example/sheinnovates/CreateAccount.java
@@ -1,13 +1,26 @@
 package com.example.sheinnovates;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccount extends AppCompatActivity {
-
+    private Button mCreateAcc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        mCreateAcc = (Button) findViewById(R.id.button);
+
+        mCreateAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), Login.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 }
