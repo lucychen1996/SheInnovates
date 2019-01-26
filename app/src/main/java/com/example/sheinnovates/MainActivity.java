34b0bbe02984abdce70e2214dc945d66c589f8f0
Mortick <qing.ch156@gmail.com>
2019-01-26 14:22:29 -0500

    America's Top Next Businesswoman

--- app/src/main/java/com/example/sheinnovates/MainActivity.java
+++ app/src/main/java/com/example/sheinnovates/MainActivity.java
@@ -4,21 +4,61 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mlogin;
+    private Button mcreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        mlogin = (Button) findViewById(R.id.login);
+        mlogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent myIntent = new Intent(v.getContext(), Login.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        mcreateAccount = (Button) findViewById(R.id.createAccount);
+        mcreateAccount.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent myIntent = new Intent(v.getContext(), CreateAccount.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
 
 
     }
+
+    public void loadScreen(View view){
+        Intent create = new Intent(this, LoadScreen.class);
+        setContentView(R.layout.activity_loadscreen);
+        startActivity(create);
+    }
+
     public void createAccount(View view) {
         // Code here executes on main thread after user presses button
         Intent create = new Intent(this, CreateAccount.class);
-//        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.activity_create_account);
+        startActivity(create);
+    }
+
+    public void Login(View view){
+        Intent create = new Intent(this, Login.class);
+        setContentView(R.layout.activity_login);
         startActivity(create);
     }
 
