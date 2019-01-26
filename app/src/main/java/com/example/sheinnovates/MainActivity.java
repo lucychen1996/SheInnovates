efe7eaf5db0a3989145e4c0e6baba9815a3f63f3
Lucy Chen <yic76@pitt.edu>
2019-01-26 11:24:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sheinnovates/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.sheinnovates;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void createAccount(View view) {
+        // Code here executes on main thread after user presses button
+        Intent create = new Intent(this, CreateAccount.class);
+//        setContentView(R.layout.activity_create_account);
+        startActivity(create);
+    }
+
+}
